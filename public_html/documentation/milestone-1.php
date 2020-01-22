5a39cae3b4186a60e5854b57b30071f8901b88ca
patrickleyba <pleyba4@cnm.edu>
2020-01-22 15:48:16 -0700

    Started PWP and created directories public_html and documentation then I created the read me, .gitignore w/vendor, and milestone-1.php file inside the documentation directory.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>NM Technology & Integration Solutions</title>
+</head>
+
+<body>
+	<div class="center">
+		<h5>Purpose:</h5> <p>This project will be a business website for NM Technology in order to attract
+			new customers that need professional Web Application Development, Company Websites, Business Management
+			Work Flow, solutions via a modern technology base to in.</p>
+
+		<h5>Audience:</h5><p></p> The NM Technology website is geared toward providing local small businesses and
+		commercial enterprises.</p>
+
+		<h5>Goal:</h5><ul>
+			<li>To attract more clients in a professional manner.</li>
+			<li>Have a professional appearance and existence on the internet.</li>
+			<li>Expand my business to have a team of developers.</li>
+		</ul>
+
+	<div class="center"><h2>Persona</h2>
+
+		<h5>Name:</h5><p>Albuquerque Pet Memorial Service</p>
+
+		<h5>Business Name:</h5><p>Pet Cremation Service Provider</p>
+
+		<h5>Profession:</h5><p>APMS provides pet cremation services for pet owners with deceased companions. They provide
+			Urn products and other commemorative products to help with the loss of a pet that is equal to a family member.</p>
+
+		<h5>Technology:</h5><p>APMS employess currently uses paper forms to process companions and this gets messy, presents errors in orders
+			and gets staff unorganized if not maintain properly.</p>
+
+		<h5>Fustrations and Needs:</h5><p>APMS is fustrated with the paper based method for processing companion cremation
+			orders from pet owners. APMS needs a digital solution in order to make their work flow more efficient, accurate
+			and faster turn-over rate for clients and veterinary clinics.</p>
+
+		<h5>Goals:</h5><ul>
+			<li>Provide a more consistent service timeline for vet clinics and pet owners.</li>
+			<li>Utilize a digital platform in order to process work efficiently.</li>
+			<li>Increase revenue.</li>
+			<li>Eliminate errors that would occur with the paper method.</li>
+		</ul>
+
+		<h5>User Story:</h5><p></p>
+		</div>
+</body>
+
+</html>
