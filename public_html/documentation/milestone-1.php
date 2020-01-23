c824d701d36b5b313af74377159d54d4bbef6cc1
patrickleyba <pleyba4@cnm.edu>
2020-01-23 08:47:52 -0700

    Started PWP and created directories public_html and documentation then I created the read me, .gitignore w/vendor, and milestone-1.php file inside the documentation directory.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,16 +2,17 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>NM Technology & Integration Solutions</title>
+	<title>My PWP nmtis.com</title>
 </head>
 
 <body>
 	<div class="center">
+		<h1>nmtis.com</h1>
 		<h5>Purpose:</h5> <p>This project will be a business website for NM Technology in order to attract
 			new customers that need professional Web Application Development, Company Websites, Business Management
 			Work Flow, solutions via a modern technology base to in.</p>
 
-		<h5>Audience:</h5><p></p> The NM Technology website is geared toward providing local small businesses and
+		<h5>Audience:</h5><p>The NM Technology website is geared toward providing local small businesses and
 		commercial enterprises.</p>
 
 		<h5>Goal:</h5><ul>
@@ -22,14 +23,16 @@
 
 	<div class="center"><h2>Persona</h2>
 
-		<h5>Name:</h5><p>Albuquerque Pet Memorial Service</p>
+		<h5>Name:</h5><p>Jennifer Jones</p>
 
-		<h5>Business Name:</h5><p>Pet Cremation Service Provider</p>
+		<h5>Business Name:</h5><p>Albuquerque Pet Memorial Service (APMS)</p>
+
+		<h5>Business Type:</h5><p>Albuquerque Pet Memorial Service</p>
 
 		<h5>Profession:</h5><p>APMS provides pet cremation services for pet owners with deceased companions. They provide
 			Urn products and other commemorative products to help with the loss of a pet that is equal to a family member.</p>
 
-		<h5>Technology:</h5><p>APMS employess currently uses paper forms to process companions and this gets messy, presents errors in orders
+		<h5>Technology:</h5><p>APMS employees currently uses paper forms to process companions and this gets messy, presents errors in orders
 			and gets staff unorganized if not maintain properly.</p>
 
 		<h5>Fustrations and Needs:</h5><p>APMS is fustrated with the paper based method for processing companion cremation
